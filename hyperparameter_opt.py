0b7e465279a0d9082ecabc4c208b3dcf1d145039
Paul-Emmanuel SOTIR <paul-emmanuel@outlook.com>
2017-08-05 14:46:10 -0400

    Implemented Xavier initialization and removed weigths std dev hyperparameter Removed harversine distance from features (todo: verify it is indeed useless) Added test_rmse and predictions to tf summary Added activation function as hyperparameter Improved hyperparameter search space

--- hyperparameter_opt.py
+++ hyperparameter_opt.py
@@ -4,7 +4,6 @@
 Uses hyperopt module to search for optimal DNN hyper parameters.
 
 TODO:
-    * avoid writing to many tf summaries
     * handle early stoping and progressively increase dataset size (or/and NN depth) during hyperparameter optimization to accelerate optmization and allow more model evaluations
 
 .. See https://github.com/PaulEmmanuelSotir/NYC_TaxiTripDuration
@@ -25,19 +24,14 @@ SUB_TRAINSET_SIZE = 0.1
 # Hyperparameter optimization space and algorithm
 MAX_EVALS = 100
 OPT_ALGO = ho.tpe.suggest
-HP_SPACE = {'lr': ho.hp.loguniform('lr', math.log(5e-6), math.log(1e-2)),
-            'depth': ho.hp.quniform('depth', 6, 9, 1),
-            'batch_size': ho.hp.choice('batch_size', [128, 256, 512]),
-            'hidden_size': ho.hp.choice('hidden_size', [64, 128, 256, 512, 1024]),
-            'weight_std_dev': ho.hp.normal('weight_std_dev', 0.1, 0.01),
-            'dropout_keep_prob': ho.hp.normal('dropout_keep_prob', 0.7, 0.03),
-            'duration_resolution': ho.hp.choice('duration_resolution', [128, 256, 512, 1024, 2048]),
-            'duration_std_margin': ho.hp.choice('duration_std_margin', [3, 4, 5])}
-
-def _correct_hyperparameters(hyperparameters):
-    hyperparameters['depth'] = int(hyperparameters['depth'])
-    hyperparameters['weight_std_dev'] = np.clip(hyperparameters['weight_std_dev'], 1e-9, 1.)
-    hyperparameters['dropout_keep_prob'] = np.clip(hyperparameters['dropout_keep_prob'], 0.05, 1.)
+HP_SPACE = {'lr': ho.hp.loguniform('lr', math.log(5e-6), math.log(1e-3)),
+            'depth': ho.hp.choice('depth', [7, 8, 9]),
+            'activation': ho.hp.choice('activation', [tf.nn.tanh, tf.nn.relu, tf.nn.elu]),
+            'batch_size': ho.hp.choice('batch_size', [256, 512, 1024]),
+            'hidden_size': ho.hp.choice('hidden_size', [128, 256, 512, 1024]),
+            'dropout_keep_prob': ho.hp.uniform('dropout_keep_prob', 0.5, 1.),
+            'duration_std_margin': ho.hp.choice('duration_std_margin', [3, 4, 5]),
+            'duration_resolution': ho.hp.choice('duration_resolution', [128, 256, 512, 1024])}
 
 def main():
     # Parse cmd arguments
@@ -48,17 +42,16 @@ def main():
     train_set_path = '/input/train.csv' if args.floyd_job else './NYC_taxi_data_2016/train.csv'
     pred_set_path = '/input/test.csv' if args.floyd_job else './NYC_taxi_data_2016/test.csv'
 
-    # Parse, preprocess data and sample a subset of trainset
+    # Load data and sample a subset of trainset
     features, predset, dataset, (target_std, target_mean) = nyc_dnn.load_data(train_set_path, pred_set_path)
     train_data, test_data, train_targets, test_targets = dataset
     train_data, train_targets = resample(train_data, train_targets, replace=False, n_samples=int(SUB_TRAINSET_SIZE * len(train_data)))
     dataset = (train_data, test_data, train_targets, test_targets)
 
-    # Define objective function optimized by hyperopt
+    # Define the objective function optimized by hyperopt
     eval_count = 0
     def _objective(hyperparameters):
-        _correct_hyperparameters(hyperparameters)
-        nonlocal eval_count
+        nonlocal eval_count # TODO: find a better way to do this (probably using hyperopt module features)
         eval_count += 1
         print('\n\n' + '#' * 10 + ' TRYING HYPERPERPARAMETER SET #' + str(eval_count) + ' ' + '#' * 10)
         print(hyperparameters)
@@ -67,7 +60,7 @@ def main():
         # Build model
         model = nyc_dnn.build_model(len(features), hyperparameters, target_std, target_mean)
         # Train model
-        model_save_dir = save_dir + str(eval_count) + '/' # TODO: find a better way to do this (probably using hyperopt module features)
+        model_save_dir = save_dir + str(eval_count) + '/'
         test_rmse = nyc_dnn.train(model, dataset, TRAINING_EPOCHS, hyperparameters, model_save_dir)
         return {'loss': test_rmse, # TODO: put last batch average loss here?
                 'true_loss': test_rmse,
@@ -81,7 +74,6 @@ def main():
                               algo=OPT_ALGO,
                               max_evals=MAX_EVALS,
                               trials=trials)
-    _correct_hyperparameters(best_parameters)
     print('\n\n' + '#' * 20 + '  BEST HYPERPARAMETERS  ' + '#' * 20)
     print(best_parameters) # TODO: translate ho.hp.choice hyperarameters from index to their actual value
 
--- nyc_dnn.py
+++ nyc_dnn.py
@@ -5,9 +5,7 @@ Note that the code could probably be greatly simplified using tf.train.Superviso
 but we prefer to define model by hand here to learn more about tensorflow python API.
 
 TODO:
-    * try batch normalization
-    * try xavier initialization
-    * save best performing model on test set instead of last one
+    * save best performing model instead of last one
 
 .. See https://github.com/PaulEmmanuelSotir/NYC_TaxiTripDuration
 """
@@ -22,31 +20,23 @@ from sklearn.model_selection import train_test_split
 
 __all__ = ['load_data', 'build_model', 'train']
 
-DEFAULT_HYPERPARAMETERS = {'lr': 8e-5,
-                           'depth': 6,
-                           'dropout_keep_prob': 0.7,
-                           'hidden_size': 512,
-                           'batch_size': 256,
-                           'weight_std_dev': 0.1,
-                           'duration_resolution': 512,
-                           'duration_std_margin': 4}
-TRAINING_EPOCHS = 250
+DEFAULT_HYPERPARAMETERS = {'hidden_size': 512, 'duration_std_margin': 5, 'depth': 9, 'lr': 0.0003, 'duration_resolution': 256, 'batch_size': 512, 'dropout_keep_prob': 0.737, 'activation': tf.nn.tanh}
+
+TRAINING_EPOCHS = 180
 DISPLAY_STEP_PREDIOD = 2
 ALLOW_GPU_MEM_GROWTH = True
-TEST_SIZE = 0.1
+TEST_SIZE = 0.07
 RANDOM_STATE = 100 # Random state for train_test_split
 
-def _haversine_np(lon1, lat1, lon2, lat2):
-    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-    a = np.sin(dlat/2.0)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2.0)**2
-    c = 2 * np.arcsin(np.sqrt(a))
-    km = 6367 * c
-    return km
+def _xavier_init(fan_in, fan_out):
+    return tf.random_normal([fan_in, fan_out], stddev=math.sqrt(3. / (fan_in + fan_out)))
 
 def _softmax_to_duration(softmax, std, mean, duration_std_margin, duration_resolution):
-    """ Inverse logistic function (logit function) """
+    """ Inverse logistic function (logit function)
+    This function is used to convert softmax output layer to a trip duration in a differentiable way so that we can perform softmax regression with L2 loss.
+    Each softmax output probability weights a different trip duration value.
+    These values are choosed to discretize trip duration so that area under gaussian curve is constant between those (follows a logit function).
+    """
     max_x = tf.exp(duration_std_margin * std) / (1. + tf.exp(duration_std_margin * std))
     min_x = tf.exp(-duration_std_margin * std) / (1. + tf.exp(-duration_std_margin * std))
     mean_indice = tf.reduce_mean(tf.multiply(softmax, tf.range(0., duration_resolution, dtype=tf.float32)), axis=1) # TODO: make sure ignoring first softmax output (multiplied by 0) isn't a problem
@@ -54,43 +44,32 @@ def _softmax_to_duration(softmax, std, mean, duration_std_margin, duration_resol
     pred = tf.log(x / (1 - x)) + mean
     return pred
 
-"""
-def _discretize_duration(y, std, mean, duration_std_margin, duration_resolution):
-    min_x = tf.exp(duration_std_margin * std) / (1. + tf.exp(duration_std_margin * std))
-    max_x = tf.exp(-duration_std_margin * std) / (1. + tf.exp(-duration_std_margin * std))
-    vals = tf.exp(tf.reshape(y, [-1]) - mean)
-    indices = ((min_x - vals / (1. + vals)) / (max_x - min_x) + 1.) * duration_resolution
-    indices = tf.clip_by_value(indices, 0., duration_resolution)
-    return tf.cast(tf.round(indices), tf.int32)
-"""
-
 def load_data(train_path, test_path):
     trainset = pd.read_csv(train_path)
     predset = pd.read_csv(test_path)
+
     # Remove outliers (huge trip durations)
     q = trainset.trip_duration.quantile(0.998)
     trainset = trainset[trainset.trip_duration < q]
+
     # Parse and vectorize dates
-    def _preprocess_date(dataset, field, vectorize=True):
+    def _preprocess_date(dataset, field):
         dataset[field] = pd.to_datetime(dataset[field])
-        if vectorize:
-            dataset[field + '_hour'] = dataset[field].dt.hour
-            dataset[field + '_min'] = dataset[field].dt.minute
-            dataset[field + '_weekday'] = dataset[field].dt.weekday
-            dataset[field + '_day'] = dataset[field].dt.day
+        dataset[field + '_hour'] = dataset[field].dt.hour
+        dataset[field + '_min'] = dataset[field].dt.minute
+        dataset[field + '_weekday'] = dataset[field].dt.weekday
+        dataset[field + '_day'] = dataset[field].dt.day
     _preprocess_date(trainset, 'pickup_datetime')
     _preprocess_date(predset, 'pickup_datetime')
-    _preprocess_date(trainset, 'dropoff_datetime', vectorize=False)
     # Vectorize flags
     trainset.store_and_fwd_flag, _ = pd.factorize(trainset.store_and_fwd_flag)
     predset.store_and_fwd_flag, _ = pd.factorize(predset.store_and_fwd_flag)
-    # Process harversine distance from longitudes and latitudes
-    trainset.radial_distance = _haversine_np(trainset.pickup_longitude, trainset.pickup_latitude, trainset.dropoff_longitude, trainset.dropoff_latitude)
-    predset.radial_distanc = _haversine_np(predset.pickup_longitude, predset.pickup_latitude, predset.dropoff_longitude, predset.dropoff_latitude)
-    # Transform target trip durations to log(trip durations) (permits to get a gaussian distribution of trip_durations, see data exploration notebook)
+
+    # Transform target trip durations to log(trip durations + 1) (permits to get a gaussian distribution of trip_durations, see data exploration notebook)
     targets = np.log(trainset.trip_duration + 1).values.reshape([-1, 1])
-    # Get trip duration mean and std dev for duration discretization
+    # Get trip duration mean and std dev for duration softmax regression
     mean, std = np.mean(targets), np.std(targets)
+
     # Remove unused feature columns
     features = [key for key in trainset.keys().intersection(predset.keys()) if key != 'id' and key != 'pickup_datetime']
     data = trainset[features].get_values()
@@ -100,55 +79,56 @@ def load_data(train_path, test_path):
     standardizer = preprocessing.StandardScaler()
     train_data = standardizer.fit_transform(train_data)
     test_data = standardizer.transform(test_data)
-    return features, (predset['id'], predset[features].get_values()), (train_data, test_data, train_targets, test_targets), (std, mean)
+    pred_data = standardizer.transform(predset[features].get_values())
+    return features, (predset['id'], pred_data), (train_data, test_data, train_targets, test_targets), (std, mean)
 
-def _dnn(X, weights, biases, dropout_keep_prob):
-    layers = [tf.nn.dropout(tf.nn.tanh(tf.add(tf.matmul(X, weights[0]), biases[0])), dropout_keep_prob)]
+def _dnn(X, weights, biases, dropout_keep_prob, activation):
+    layers = [tf.nn.dropout(activation(tf.add(tf.matmul(X, weights[0]), biases[0])), dropout_keep_prob)]
     for w, b in zip(weights[1:-1], biases[1:-1]):
-        dense = tf.nn.dropout(tf.nn.tanh(tf.add(tf.matmul(layers[-1], w), b)), dropout_keep_prob)
+        dense = tf.nn.dropout(activation(tf.add(tf.matmul(layers[-1], w), b)), dropout_keep_prob)
         dense_bn = tf.layers.batch_normalization(dense)
         layers.append(dense_bn)
-    logits = tf.add(tf.matmul(layers[-1], weights[-1]), biases[-1], name='output')
-    return logits
+    return tf.add(tf.matmul(layers[-1], weights[-1]), biases[-1], name='logits')
 
-def build_model(n_input, hyperparameters, target_std, target_mean):
-    # Define placeholders
+def build_model(n_input, hp, target_std, target_mean):
+    # Input placeholders
     X = tf.placeholder(tf.float32, [None, n_input], name='X')
     y = tf.placeholder(tf.float32, [None, 1], name='y')
     dropout_keep_prob = tf.placeholder(tf.float32, name='dropout_keep_prob')
 
     with tf.name_scope('dnn'):
-        # Define MLP weights and biases variables
+        # Declare tensorflow constants (hyperparameters)
         target_std, target_mean = tf.constant(target_std, dtype=tf.float32), tf.constant(target_mean, dtype=tf.float32)
-        hidden_size, weight_std_dev, resolution, std_margin = hyperparameters['hidden_size'], hyperparameters['weight_std_dev'], tf.constant(hyperparameters['duration_resolution'], dtype=tf.float32), tf.constant(hyperparameters['duration_std_margin'], dtype=tf.float32)
-        weights = [tf.Variable(tf.random_normal([n_input, hidden_size], stddev=weight_std_dev), name='w1')]
+        hidden_size, resolution, std_margin = hp['hidden_size'], tf.constant(hp['duration_resolution'], dtype=tf.float32), tf.constant(hp['duration_std_margin'], dtype=tf.float32)
+        # Define DNN weight and bias variables
+        weights = [tf.Variable(_xavier_init(n_input, hidden_size), name='w1')]
         biases = [tf.Variable(tf.random_normal([hidden_size]), name='b1')]
-        for i in range(1, hyperparameters['depth'] - 1):
-            weights.append(tf.Variable(tf.random_normal([hidden_size, hidden_size], stddev=weight_std_dev), name='w' + str(i)))
+        for i in range(1, hp['depth'] - 1):
+            weights.append(tf.Variable(_xavier_init(hidden_size, hidden_size), name='w' + str(i)))
             biases.append(tf.Variable(tf.random_normal([hidden_size]), name='b' + str(i)))
-        weights.append(tf.Variable(tf.random_normal([hidden_size, hyperparameters['duration_resolution']], stddev=weight_std_dev), name='w_out'))
+        weights.append(tf.Variable(_xavier_init(hidden_size, hp['duration_resolution']), name='w_out'))
         biases.append(tf.Variable(tf.random_normal([1]), name='b_out'))
         # Build fully connected layers
-        logits = _dnn(X, weights, biases, dropout_keep_prob)
-    # Add variables to summary
-    for v in [*biases, *weights]:
-        tf.summary.histogram(v.name, v)
+        logits = _dnn(X, weights, biases, dropout_keep_prob, hp['activation'])
+
     # Define loss and optimizer
     pred = _softmax_to_duration(tf.nn.softmax(logits), target_std, target_mean, std_margin, resolution)
     rmse = tf.sqrt(tf.losses.mean_squared_error(y, tf.reshape(pred, [-1, 1])))
-    optimizer = tf.train.AdamOptimizer(learning_rate=hyperparameters['lr']).minimize(rmse)
-    tf.summary.scalar('rmse', rmse)
-    # Create model saver
-    saver = tf.train.Saver()
+    optimizer = tf.train.AdamOptimizer(learning_rate=hp['lr']).minimize(rmse)
     # Variable initialization operation
     init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
-    return pred, rmse, optimizer, (X, y, dropout_keep_prob), saver, init_op
+    return pred, rmse, optimizer, (X, y, dropout_keep_prob), tf.train.Saver(), init_op
 
-def train(model, dataset, epochs, hyperparameters, save_dir=None, predset=None):
+def train(model, dataset, epochs, hp, save_dir=None, predset=None):
     # Unpack parameters
     train_data, test_data, train_targets, test_targets = dataset
     pred, rmse, optimizer, placeholders, saver, init_op = model
     X, y, dropout_keep_prob = placeholders
+
+    # Add pred and rmse to summary
+    tf.summary.histogram('pred', pred)
+    rmse_summary_op = tf.summary.scalar('rmse', rmse)
+
     # Start tensorlfow session
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = ALLOW_GPU_MEM_GROWTH
@@ -159,31 +139,29 @@ def train(model, dataset, epochs, hyperparameters, save_dir=None, predset=None):
         if save_dir is not None:
             summary_op = tf.summary.merge_all()
             summary_writer = tf.summary.FileWriter(save_dir, sess.graph)
+            summary_test_writer = tf.summary.FileWriter(save_dir, sess.graph)
 
         # Training loop
         for epoch in range(epochs):
-            #avg_loss = 0. # TODO: remove it
-            batch_count = len(train_data) // hyperparameters['batch_size']
+            batch_count = len(train_data) // hp['batch_size']
             for _ in range(batch_count):
-                indices = np.random.randint(len(train_data), size=hyperparameters['batch_size'])
+                indices = np.random.randint(len(train_data), size=hp['batch_size'])
                 batch_xs = train_data[indices, :]
                 batch_ys = train_targets[indices, :]
-                # Fit using batched data
-                sess.run(optimizer, feed_dict={X: batch_xs, y: batch_ys, dropout_keep_prob: hyperparameters['dropout_keep_prob']})
-                # TODO: find a cheap way to get average loss
+                sess.run(optimizer, feed_dict={X: batch_xs, y: batch_ys, dropout_keep_prob: hp['dropout_keep_prob']})
             # Display progress
             if epoch % DISPLAY_STEP_PREDIOD == 0:
                 if save_dir is not None:
                     summary, last_loss = sess.run([summary_op, rmse], feed_dict={X: batch_xs, y: batch_ys, dropout_keep_prob: 1.})
                     summary_writer.add_summary(summary, epoch)
+                    summary, test_rmse = sess.run([rmse_summary_op, rmse], feed_dict={X: test_data, y: test_targets, dropout_keep_prob: 1.})
+                    summary_test_writer.add_summary(summary, epoch)
                 else:
                     last_loss = sess.run(rmse, feed_dict={X: batch_xs, y: batch_ys, dropout_keep_prob: 1.})
-                test_rmse = sess.run(rmse, feed_dict={X: test_data, y: test_targets, dropout_keep_prob: 1.})
+                    test_rmse = sess.run(rmse, feed_dict={X: test_data, y: test_targets, dropout_keep_prob: 1.})
                 print("Epoch=%03d/%03d, last_loss=%.6f, test_rmse=%.6f" % (epoch, epochs, last_loss, test_rmse))
-        # Calculate test RMSE
-        print("Training done, testing...")
-        test_rmse = sess.run(rmse, feed_dict={X: test_data, y: test_targets, dropout_keep_prob: 1.})
-        print("test_rmse=%.8f" % (test_rmse))
+        print("Training done.")
+
         # Save model
         if save_dir is not None:
             print('Saving model...')
@@ -191,7 +169,7 @@ def train(model, dataset, epochs, hyperparameters, save_dir=None, predset=None):
         if predset is not None:
             print('Applying trained model on prediction set')
             predictions = []
-            for batch in np.array_split(predset, len(predset) // hyperparameters['batch_size']):
+            for batch in np.array_split(predset, len(predset) // hp['batch_size']):
                 predictions.extend(sess.run(pred, feed_dict={X: batch, dropout_keep_prob: 1.}).flatten())
             return test_rmse, predictions
         return test_rmse
@@ -211,6 +189,10 @@ def main():
     # Build model
     hyperparameters = DEFAULT_HYPERPARAMETERS
     model = build_model(len(features), hyperparameters, target_std, target_mean)
+    
+    # Add trainable variables to summary
+    for v in tf.trainable_variables():
+        tf.summary.histogram(v.name, v)
 
     # Train model
     print('Model built, starting training.')
@@ -222,3 +204,13 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+"""
+def _discretize_duration(y, std, mean, duration_std_margin, duration_resolution):
+    min_x = tf.exp(duration_std_margin * std) / (1. + tf.exp(duration_std_margin * std))
+    max_x = tf.exp(-duration_std_margin * std) / (1. + tf.exp(-duration_std_margin * std))
+    vals = tf.exp(tf.reshape(y, [-1]) - mean)
+    indices = ((min_x - vals / (1. + vals)) / (max_x - min_x) + 1.) * duration_resolution
+    indices = tf.clip_by_value(indices, 0., duration_resolution)
+    return tf.cast(tf.round(indices), tf.int32)
+"""
